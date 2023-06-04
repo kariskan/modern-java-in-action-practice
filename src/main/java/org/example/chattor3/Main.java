c776f89532c07882e7ef5e1c8660c2f5b41c4f20
kariskan <zkfzpf56@naver.com>
2023-06-04 14:46:02 +0900

    feat: sort step 2

--- src/main/java/org/example/chattor3/Main.java
+++ src/main/java/org/example/chattor3/Main.java
@@ -3,8 +3,8 @@ package org.example.chattor3;
 import static org.example.domain.Color.RED;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import org.example.chattor3.applecomparator.AppleComparator;
 import org.example.domain.Apple;
 
 public class Main {
@@ -15,7 +15,12 @@ public class Main {
 		inventory.add(new Apple(3, RED));
 		inventory.add(new Apple(2, RED));
 		inventory.add(new Apple(1, RED));
-		inventory.sort(new AppleComparator());
+		inventory.sort(new Comparator<Apple>() {
+			@Override
+			public int compare(Apple o1, Apple o2) {
+				return o1.getWeight() - o2.getWeight();
+			}
+		});
 	}
 
 }
